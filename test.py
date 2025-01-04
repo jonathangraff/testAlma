feeb8d02e5a1d2d7760f1dc57291dbcc1a850b57
jonathangraff <jonathan.graff@hotmail.fr>
2025-01-04 14:23:23 +0100

    modified the context manager

--- test.py
+++ test.py
@@ -2,23 +2,25 @@ from enum import Enum
 from random import random
 import threading, logging
 from time import sleep, time
+from contextlib import contextmanager
+
 
 REAL_LIFE_SECOND = 0.001
 
 
+def wait(t: float):
+   """This function theoretically waits for t seconds, practically it applies the factor REAL_LIFE_SECOND."""
+   sleep(t * REAL_LIFE_SECOND)
+
+
 class Action(Enum):
-   IDLE = "Idle"
+   IDLE = "Being idle"
    MINING_FOO = "Mining Foo"
    MINING_BAR = "Mining Bar"
    ASSEMBLING = "Assembling a Foo and a Bar for a Foobar"
    SELLING = "Selling Foobar"
    BUYING = "Buying a new Robot"
    CHANGING_ACTIVITY = "Changing Activity"
-
-
-def wait(t: float):
-   """This function waits for t seconds, applying the factor REAL_LIFE_SECOND."""
-   sleep(t * REAL_LIFE_SECOND)
 
 
 class Robot(threading.Thread):
@@ -32,25 +34,6 @@ class Robot(threading.Thread):
     lock = threading.Lock()
     robots = []
 
-    class activity:
-        """Crée un context manager pour uniformiser les différentes actions des robots"""
-        def __init__(self, robot, action: Action, waiting_time: float, message: str):
-            self.__robot = robot
-            self.__action = action
-            self.__waiting_time = waiting_time
-            self.__message = message
-
-        def __enter__(self):
-            self.__robot.change_status(self.__action)
-            wait(self.__waiting_time)
-            Robot.lock.acquire()
-
-        def __exit__(self, a, b, c):
-            Robot.lock.release()
-            self.__robot.last_action = self.__action
-            logging.info(self.__message)
-            self.__robot.change_status(Action.IDLE)
-
     def __init__(self):
         logging.info("Initialization of new robot")
         super().__init__(target=choose_task_to_do_if_idle, args=(self,))
@@ -62,41 +45,59 @@ class Robot(threading.Thread):
 
     def change_status(self, action: Action):
         logging.info(f"Robot {self.id} : {action.value}")
-        logging.info(get_resources_str())
+        logging.info(get_resources_string())
         self.status = action
 
+    @contextmanager
+    def activity(self, action: Action, waiting_time: float, message: str =""):
+        """Creates a context manager to uniformize the different actions of the robots"""
+
+        self.change_status(action)
+        wait(waiting_time)
+        Robot.lock.acquire()
+        try:
+            yield
+        finally:
+            Robot.lock.release()
+            self.last_action = action
+            if message:
+                logging.info(f"Robot {self.id} " + message)
+            self.change_status(Action.IDLE)
+
     def mine_foo(self):
-        with Robot.activity(self, Action.MINING_FOO, 1, f"Robot {self.id} MINED 1 FOO"):
+        with self.activity(Action.MINING_FOO, waiting_time=1, message="MINED 1 FOO"):
             Robot.resources["foo"] += 1
 
     def mine_bar(self):
-        with Robot.activity(self, Action.MINING_BAR, random() * 1.5 + 0.5, f"Robot {self.id} MINED 1 BAR"):
+        with self.activity(Action.MINING_BAR, waiting_time=random() * 1.5 + 0.5, message="MINED 1 BAR"):
             Robot.resources["bar"] += 1
 
     def assemble(self):
-        with Robot.activity(self, Action.ASSEMBLING, 2, f"Robot {self.id} TRIED TO ASSEMBLE 1 FOOBAR"):
-            if random() < 0.6: # Success case
+        with self.activity(Action.ASSEMBLING, waiting_time=2):
+            if random() < 0.6:      # Success case
                 Robot.resources["foobar"] += 1
-                message = f"ROBOT {self.id} ASSEMBLING SUCCESS"
+                message = f"ROBOT {self.id} SUCCESSFULLY ASSEMBLED 1 FOOBAR"
             else:
                 Robot.resources["bar"] += 1
-                message = f"ROBOT {self.id} ASSEMBLING FAILURE"
+                message = f"ROBOT {self.id} FAILED TO ASSEMBLE 1 FOOBAR"
         logging.info(message)
 
     def sell(self, nb_foobar: int):
-        with Robot.activity(self, Action.SELLING, 10, f"ROBOT {self.id} SOLD {nb_foobar} FOOBAR"):
+        with self.activity(Action.SELLING, waiting_time=10, message=f"SOLD {nb_foobar} FOOBAR"):
             Robot.resources["euros"] += nb_foobar
         
     def buy_robot(self):
-        with Robot.activity(self, Action.BUYING, 1, f"ROBOT {self.id} BOUGHT A ROBOT"):
+        with self.activity(Action.BUYING, waiting_time=1, message="BOUGHT A ROBOT"):
             Robot()
 
     def change_activity(self):
-        with Robot.activity(self, Action.CHANGING_ACTIVITY, 5, f"ROBOT {self.id} CHANGED ACTIVITY"):
+        with self.activity(Action.CHANGING_ACTIVITY, waiting_time=5, message="CHANGED ACTIVITY"):
             pass
 
-def get_resources_str():
-    return f"foo : {Robot.resources["foo"]}   bar : {Robot.resources["bar"]}   foobar : {Robot.resources["foobar"]}   euros : {Robot.resources["euros"]}   Nb Robots : {len(Robot.robots)}."
+
+def get_resources_string():
+    res=Robot.resources
+    return f"foo : {res["foo"]}   bar : {res["bar"]}   foobar : {res["foobar"]}   euros : {res["euros"]}   Nb Robots : {len(Robot.robots)}."
 
 
 def choose_task_to_do_if_idle(robot: Robot):
@@ -126,15 +127,16 @@ def choose_task_to_do_if_idle(robot: Robot):
 
 
 def main():
-    started_at = time()
+    start = time()
     Robot()
     Robot()
     for r in Robot.robots:
         r.join()
-    total_time = (time() - started_at)/REAL_LIFE_SECOND
+    total_time = (time() - start)/REAL_LIFE_SECOND
     print(f"{total_time = }")
 
 if __name__=="__main__":
     format = "%(message)s"
     logging.basicConfig(format=format, level=logging.INFO)
     main()
+    
